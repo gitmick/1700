df15036eca5229f0df8621bb289acc495359cf9d
a-d-a-p-t-e-r <scannedforcontent@gmail.com>
2014-10-22 18:18:29 +0200

    Phil was here

---
+++ 1700Easel/assets/Phil/descriptor.js
@@ -0,0 +1,15 @@
+function Phil(){
+	this.thrower = new Image();
+}
+
+Phil.prototype = new Asset();
+
+Phil.prototype.load = function () {
+	this.loadImage("Thrower.png", this.thrower);
+};
+
+Phil.prototype.drawInitial = function() {
+	this.displayEntity.addBitmap(this.thrower, true);
+	this.displayEntity.pos(this.startX, this.startY);
+	//this.setAction(new ThrowAction());
+};
