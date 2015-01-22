885acf19688ac3a7c6ae92fa4d1bd62911deffcd
Ivo Francx <ivofrancx@gmail.com>
2015-01-22 12:04:09 +0100

    Web update

--- 1700Easel/WebPage/js/script.js
+++ 1700Easel/WebPage/js/script.js
@@ -1,11 +1,68 @@
+var allNewsArticles = [];
+var displayedNewsArticles = [];
+var newsPage = 1;
+
+getAllNewsArticles();
+showNewsArticles();
+getDisplayedNewsArticles();
+
+
+function getAllNewsArticles() {
+	for (var i=1 ; document.getElementById("news_" + i) != null; i++) {
+		 allNewsArticles.push(document.getElementById("news_" + i))
+	}
+	
+}
+
+function showNewsArticles() {
+	for (var i=1 ; i<allNewsArticles.length-3; i++) {
+		document.getElementById("news_" + i).classList.add("hidden");
+	}
+}
+
+function getDisplayedNewsArticles() {
+	
+	var a = allNewsArticles.length - 4*newsPage + 1;
+	
+	for (var i=1 ; i<allNewsArticles.length+1; i++) {
+		var x = document.getElementById("news_" + i);
+		x.classList.add("hidden");
+		x.className = x.className + " hidden";
+	    x.classList.add("hidden");
+	}
+	
+	for (var i = a; i<a+4; i++) {
+		document.getElementById("news_" + i).classList.remove("hidden");
+	}
+}
+
+
 function showOlder() {
-    var a = document.getElementById("news_1");
-//    a.className = a.className + " hiddenNews";
-    a.classList.add("hiddenNews");
+	newsPage+=1;
+	getDisplayedNewsArticles();
+	if(newsPage>1) {
+		document.getElementById("newer").classList.remove("hidden");
+	}
 }
 
 function showNewer() {
-	var e = document.getElementById("news_1");
-	e.classList.remove("hiddenNews");
-	
+	newsPage-=1;
+	getDisplayedNewsArticles();
+	if (newsPage==1) {
+		var x = document.getElementById("newer");
+		x.classList.add("hidden");
+		x.className = x.className + " hidden";
+	    x.classList.add("hidden");
+	}
+}
+
+
+function isIn(a, obj) {
+    var i = a.length;
+    while (i--) {
+       if (a[i]  === obj ) {
+           return true;
+       }
+    }
+    return false;
 }
